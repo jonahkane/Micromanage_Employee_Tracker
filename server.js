eb36d0f9d80c470df9639e217379c3eb05e53d8f
Jonah <jonahtkane@gmail.com>
2023-03-24 19:51:23 -0500

    updated code in server.js. built functions and repositioned commented out code to appropriate fucntion lines to indicate what is still needed

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ const db = mysql.createConnection(
     console.log(`Connected to the employee_db database.`)
   );
   
-  const questionPrompt = () => {
+  const mainMenu = () => {
     return inquirer.prompt([
         {
             type: 'list',
-            name: 'menu',
+            name: 'Main menu',
             message: 'What would you like to do?',
             choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
         }])
@@ -52,24 +52,54 @@ const db = mysql.createConnection(
                 case 'update an employee role':
                     updateEmployeeRole();
                     break;
-                    default:
-                        mainMenu();
+                case 'exit':
+                    exit();
             }})}
-                    
-// GIVEN a command-line application that accepts user input
-// WHEN I start the application
-// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+
+
+            const viewDepartments = () => {
 // WHEN I choose to view all departments
 // THEN I am presented with a formatted table showing department names and department ids
+
+            }
+
+            const viewRoles = () => {
 // WHEN I choose to view all roles
 // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+
+            }
+
+            const viewEmployees = () => {
 // WHEN I choose to view all employees
 // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+
+}
+
+const addDepartment = () => {
+    
+    // WHEN I choose to add a department
+    // THEN I am prompted to enter the name of the department and that department is added to the database
+            }
+
+            const addRole = () => {
+                // WHEN I choose to add a role
+                // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+
+            }
+
+            const addEmployee = () => {
+
+                // WHEN I choose to add an employee
+                // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+            }
+
+            const updateEmployeeRole = () => {
+                // WHEN I choose to update an employee role
+                // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+
+            }
+
+            const exit = () => {
+
+            }
+                                                                                                                    
