412f841be2e5f630b13a341d72744a73904872c5
Jonah <jonahtkane@gmail.com>
2023-03-24 19:52:44 -0500

    reformatted code

--- server.js
+++ server.js
@@ -57,19 +57,19 @@ const db = mysql.createConnection(
             }})}
 
 
-            const viewDepartments = () => {
+const viewDepartments = () => {
 // WHEN I choose to view all departments
 // THEN I am presented with a formatted table showing department names and department ids
 
             }
 
-            const viewRoles = () => {
+const viewRoles = () => {
 // WHEN I choose to view all roles
 // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 
             }
 
-            const viewEmployees = () => {
+const viewEmployees = () => {
 // WHEN I choose to view all employees
 // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 
@@ -77,29 +77,29 @@ const db = mysql.createConnection(
 
 const addDepartment = () => {
     
-    // WHEN I choose to add a department
-    // THEN I am prompted to enter the name of the department and that department is added to the database
+// WHEN I choose to add a department
+// THEN I am prompted to enter the name of the department and that department is added to the database
             }
 
-            const addRole = () => {
-                // WHEN I choose to add a role
-                // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-
-            }
-
-            const addEmployee = () => {
-
-                // WHEN I choose to add an employee
-                // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-            }
-
-            const updateEmployeeRole = () => {
-                // WHEN I choose to update an employee role
-                // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+const addRole = () => {
+// WHEN I choose to add a role
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 
             }
 
-            const exit = () => {
+const addEmployee = () => {
+
+// WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+            }
+
+const updateEmployeeRole = () => {
+// WHEN I choose to update an employee role
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+
+            }
+
+const exit = () => {
 
             }
                                                                                                                     
