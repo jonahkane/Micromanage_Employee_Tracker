23438498111d009d702d9f9b2d648f21c28129ed
Jonah <jonahtkane@gmail.com>
2023-03-24 19:25:19 -0500

    changed index.js to server.js file name

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const express = require('express');
 const mysql = require('mysql2');
 
+const PORT = process.env.PORT || 3001;
 const app = express()
 
 app.use(express.urlencoded({ extended: false }));
